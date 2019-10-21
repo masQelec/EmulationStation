978150993f8a0a8122e31f07e661e9f495e7f5ee
masQchips <masqchips@gmail.com>
2019-10-21 22:58:28 +0200

    Update SystemView.cpp

--- es-app/src/views/SystemView.cpp
+++ es-app/src/views/SystemView.cpp
@@ -8,6 +8,8 @@
 #include "Settings.h"
 #include "SystemData.h"
 #include "Window.h"
+#include "Scripting.h"
+#include "platform.h"
 
 // buffer values for scrolling velocity (left, stopped, right)
 const int logoBuffersLeft[] = { -5, -2, -1 };
@@ -201,6 +203,17 @@ bool SystemView::input(InputConfig* config, Input input)
 			mWindow->renderScreenSaver();
 			return true;
 		}
+		if(config->isMappedTo("b", input))
+		{
+			Window* window = mWindow;
+			window->pushGui(new GuiMsgBox(window, "¿ESTA SEGURO QUE DESEA SALIR?", "SI",
+				[window] {
+				Scripting::fireEvent("quit");
+				quitES();
+			}, "NO", nullptr)
+			);
+			return true;
+		}
 	}
 
 	return GuiComponent::input(config, input);
@@ -383,7 +396,9 @@ std::vector<HelpPrompt> SystemView::getHelpPrompts()
 
 	if (!UIModeController::getInstance()->isUIModeKid() && Settings::getInstance()->getBool("ScreenSaverControls"))
 		prompts.push_back(HelpPrompt("select", "lanzar salva pantallas"));
-
+	
+	prompts.push_back(HelpPrompt("b", "salir"));
+	
 	return prompts;
 }
 
